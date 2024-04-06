a98f0a66de7516ed61a26a9b7b398766e9aac87a
TanasinLnwza <tanasinlnwza@gmail.com>
2024-04-06 20:51:05 +0700

    asd

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,5 +1,12 @@
 // pages/api/hello.js
 
 export default function handler(req, res) {
-  res.status(200).json({ message: 'Hello from Next.js API!' });
+  if (req.method === 'GET') {
+    // รับคำขอ GET
+    res.status(200).json({ message: 'Hello from Next.js API!' });
+  } else {
+    // สำหรับวิธีการอื่น ๆ ที่ไม่ใช่ GET
+    res.setHeader('Allow', ['GET']); // ให้ส่งคำอนุญาตสำหรับ GET เท่านั้น
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
