7eb2e67e62f394315e5b7801705a72c9879fb4a2
TanasinLnwza <tanasinlnwza@gmail.com>
2024-04-28 20:07:33 +0700

    d

--- component/boxpost/boxpost.js
+++ component/boxpost/boxpost.js
@@ -1,22 +1,12 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import React from "react";
+import styles from "./Styles.module.css";
+function Boxpost() {
 
-// Import Swiper styles
-import 'swiper/css';
-
-export default () => {
   return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={3}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      ...
-    </Swiper>
+    <div style={{ zIndex: 1 }}>
+      sdf
+    </div>
   );
-};
+}
+
+export default Boxpost;
