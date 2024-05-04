3c20e4e1ab8757460ac2333671df0d84e3e50854
TanasinLnwza <tanasinlnwza@gmail.com>
2024-05-05 01:19:34 +0700

    hamberger categort hover

--- components/leftbar/hambergerleftbar.js
+++ components/leftbar/hambergerleftbar.js
@@ -1,36 +1,39 @@
 import React, { useState } from "react";
 
-const Hambergerleftbar = ({onCategoryChange}) => {
+const HambergerLeftBar = ({ onCategoryChange }) => {
   const [isOpen, setIsOpen] = useState(false);
-  const handleCategory = (number) => { // Corrected function name
-    onCategoryChange(number); // Update Category state with the selected number
+
+  const handleCategory = (number) => {
+    onCategoryChange(number);
   };
+
   return (
     <nav className="mr-2">
       <div className="">
         <div className="lg:hidden">
           <button
-            onClick={() => setIsOpen(!isOpen)}
+            onMouseEnter={() => setIsOpen(true)}
+            onMouseLeave={() => setIsOpen(false)}
             className="text-white focus:outline-none focus:text-white"
           >
-            <i class="fa-solid fa-bars"></i>
+            <i className="fa-solid fa-bars"></i>
           </button>
         </div>
       </div>
       {isOpen && (
-        <div className="absolute shadow-md lg:hidden ">
-            <div className="text-sm font-bold  p-2 rounded-md shadow-lg" style={{background:"rgba(51, 51, 51, 1)"}}>Category</div>
-          <div className=" bg-white rounded-md p-2 flex flex-col text-sm">
-            <button className=" text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeA")}>
+        <div className="absolute shadow-md lg:hidden">
+          <div className="text-sm font-bold p-2 rounded-md shadow-lg" style={{ background: "rgba(51, 51, 51, 1)" }}>Category</div>
+          <div className="bg-white rounded-md p-2 flex flex-col text-sm">
+            <button className="text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeA")}>
               Type A
             </button>
-            <button className=" text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeB")}>
+            <button className="text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeB")}>
               Type B
             </button>
-            <button className=" text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeC")}>
+            <button className="text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeC")}>
               Type C
             </button>
-            <button className=" text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeD")}>
+            <button className="text-black rounded-md p-2 shadow-md" onClick={() => handleCategory("typeD")}>
               Type D
             </button>
           </div>
@@ -40,4 +43,4 @@ const Hambergerleftbar = ({onCategoryChange}) => {
   );
 };
 
-export default Hambergerleftbar;
+export default HambergerLeftBar;
